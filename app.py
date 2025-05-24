a69952a933acec6ded4da11aade0ed84a36117fe
Mauricio Lima <mauriciodlima22@gmail.com>
2025-05-24 12:13:48 -0300

    Adicionando o projeto ao git

---
+++ app.py
@@ -0,0 +1,17 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+restaurante_praca = Restaurante('praça', 'gourmet')
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'grande')
+prato_paozinho = Prato('Paozinho', 2.0, 'o melhor pão da cidade')
+restaurante_praca.adicionar_bebida_cardipio(bebida_suco)
+restaurante_praca.adicionar_prato_cardipio(prato_paozinho)
+
+
+def main():
+    print(bebida_suco)
+    print(prato_paozinho)
+
+if __name__ == '__main__':
+    main()
---
+++ modelos/restaurante.py
@@ -0,0 +1,50 @@
+from modelos.avaliacao import Avaliacao
+
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        self._avaliacao = []
+        self.cardapio = []
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f"{'Nome do restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | {'Avaliacao'.ljust(20)} |{'Status'}")
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {str(restaurante.media_avaliacao()).ljust(20)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '✔️' if self._ativo else '❌'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
+    def receber_avaliacao(self, cliente, nota):
+        if nota >= 0 or nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return '-'
+
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_notas, 1)
+        return media
+    
+    def adicionar_bebida_cardipio(self, bebida):
+        self._categoria.append(bebida)
+
+    def adicionar_prato_cardipio(self, prato):
+        self._categoria.append(prato)
+
+
